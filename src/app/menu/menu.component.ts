65e29874e4dc3b906804f4a9d986f8e5bd5c99a1
sevy07 <sevy07@msn.com>
2017-11-12 17:47:38 +0000

    setting up the navigation

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,28 +7,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-
-
-
   menuState: boolean;
-
-  public menuList: any[] = [
-    { 'title': 'Home', 'link': '#' },
-    { 'title': 'Our story', 'link': '#' },
-    { 'title': 'The proposal', 'link': '#' },
-    { 'title': 'The big day', 'link': '#' },
-    {
-      'title': 'Your trip to Santiago', 'link': '#',
-      'subItems': [
-        { 'title': 'Getting there', 'link': '#' },
-        { 'title': 'Where to sleep', 'link': '#' },
-        { 'title': 'Where to eat', 'link': '#' },
-        { 'title': 'Where to drink', 'link': '#' },
-        { 'title': 'A Costa da Morte', 'link': '#' }
-      ]
-    },
-    { 'title': 'Contact us', 'link': '#' }
-  ];
+  expand: boolean;
 
   constructor() {
 
@@ -36,6 +16,7 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
     this.menuState = false;
+    this.expand = false;
   }
 
   menuToggle() {
@@ -46,12 +27,8 @@ export class MenuComponent implements OnInit {
     this.menuState = false;
   }
 
-  onItemClick(item: any) {
-    // alert(item);
-  }
-
-  toggleSubMenu(item: any) {
-    item.expand = !item.expand;
+  toggleSubMenu() {
+    this.expand = !this.expand;
   }
 
 }
--- src/app/your-trip/your-trip.component.ts
+++ src/app/your-trip/your-trip.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-your-trip',
   templateUrl: './your-trip.component.html',
   styleUrls: ['./your-trip.component.scss']
 })
-export class YourTripComponent implements OnInit {
+export class YourTripComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  private fragment: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.fragment.subscribe(fragment => { this.fragment = fragment; this.scrollIntoView(); });
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollIntoView();
+  }
+
+  scrollIntoView() {
+    try {
+      document.querySelector('#' + this.fragment).scrollIntoView();
+    } catch (e) { }
   }
 
 }
