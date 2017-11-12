fb1b5fb729f99c016ab3f1d2cb245d87fd53634d
sevy07 <sevy07@msn.com>
2017-11-12 12:17:13 +0000

    adding font-awesome and removing ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
 import { CountDownComponent } from './count-down/count-down.component';
 import { HomeComponent } from './home/home.component';
@@ -28,8 +26,7 @@ import { MenuComponent } from './menu/menu.component';
     MenuComponent
   ],
   imports: [
-    BrowserModule,
-    NgbModule.forRoot()
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
