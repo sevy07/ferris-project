b8095626d9be767e45fb94e5618839d35985d20f
sevy07 <sevy07@msn.com>
2017-11-12 12:17:54 +0000

    cleaning menu component and creating variables for sass

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,19 +7,32 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  @Input() menulist: any;
-  @Output('open')
-  open: EventEmitter<number> = new EventEmitter<number>();
-  @Output('close')
-  close: EventEmitter<number> = new EventEmitter<number>();
-  @Output('onItemSelect')
-  onItemSelect: EventEmitter<number> = new EventEmitter<number>();
+
+
+
   private menuState: boolean;
   private targetElement: any;
-  private overlayElem: any;
+
+  public menuList: any[] = [
+    { 'title': 'Home', 'link': '#' },
+    { 'title': 'Our story', 'link': '#' },
+    { 'title': 'The proposal', 'link': '#' },
+    { 'title': 'The big day', 'link': '#' },
+    {
+      'title': 'Your trip to Santiago', 'link': '#',
+      'subItems': [
+        { 'title': 'Getting there', 'link': '#' },
+        { 'title': 'Where to sleep', 'link': '#' },
+        { 'title': 'Where to eat', 'link': '#' },
+        { 'title': 'Where to drink', 'link': '#' },
+        { 'title': 'A Costa da Morte', 'link': '#' }
+      ]
+    },
+    { 'title': 'Contact us', 'link': '#' }
+  ];
 
   constructor() {
-    this.addOverlayElement();
+
   }
 
   ngOnInit() {
@@ -28,44 +41,18 @@ export class MenuComponent implements OnInit {
 
   private menuToggle() {
     this.menuState = !this.menuState;
-    this.toggleOverlay();
-    if (this.menuState) {
-      this.open.emit();
-    } else {
-      this.close.emit();
-    }
-  }
-  private closeMenu() {
-    this.menuState = false;
-    this.overlayElem.style['opacity'] = 0;
-  }
-  private onItemClick(item: any) {
-    this.onItemSelect.emit(item);
-  }
-  private toggleSubMenu(item: any) {
-    item.expand = !item.expand;
-  }
-  private addOverlayElement() {
-    this.overlayElem = document.createElement('div');
-    this.overlayElem.classList.add('cuppa-menu-overlay');
-    this.overlayElem.style['position'] = 'fixed';
-    this.overlayElem.style['background'] = 'rgba(0, 0, 0, 0.7)';
-    this.overlayElem.style['top'] = 0;
-    this.overlayElem.style['left'] = 0;
-    this.overlayElem.style['right'] = 0;
-    this.overlayElem.style['bottom'] = 0;
-    this.overlayElem.style['opacity'] = 0;
-    this.overlayElem.style['pointer-events'] = 'none';
-    this.overlayElem.style['transition'] = 'all .2s linear';
-    document.getElementsByTagName('body')[0].appendChild(this.overlayElem);
   }
 
-  private toggleOverlay() {
-    if (this.overlayElem.style['opacity'] === 0) {
-      this.overlayElem.style['opacity'] = 1;
-    } else if (this.overlayElem.style['opacity'] === 1) {
-      this.overlayElem.style['opacity'] = 0;
-    }
+  private closeMenu() {
+    this.menuState = false;
+  }
+
+  private onItemClick(item: any) {
+    // alert(item);
+  }
+
+  private toggleSubMenu(item: any) {
+    item.expand = !item.expand;
   }
 
 }
